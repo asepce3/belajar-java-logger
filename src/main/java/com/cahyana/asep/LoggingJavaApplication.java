dfaff7916b8bc7a5f7a935b25cffde28e1b581af
Asep Cahyana <asep.ce3@gmail.com>
2021-11-07 10:15:29 +0700

    Test Log

---
+++ src/main/java/com/cahyana/asep/LoggingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cahyana.asep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cahyana/asep/LoggingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahyana.asep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
